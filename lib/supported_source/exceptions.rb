aeafb33dbd3c886b769080936187b628eb6a8c51
J. R. Pickhardt <pickhardt@gmail.com>
2016-06-27 20:38:59 -0700

    More readable exceptions by adding newlines

--- lib/supported_source/exceptions.rb
+++ lib/supported_source/exceptions.rb
@@ -4,10 +4,10 @@ module SupportedSource
 
   module Exceptions
     def Exceptions.help_message
-      "\n * " + ["To get client tokens, run `supso update`.",
+      "\n\n * " + ["To get client tokens, run `supso update`.",
         "If you do not have the supso command line interface yet, first run `gem install supso`.",
         "Visit http://supportedsource.org/help for further help.",
-      ].join("\n * ")
+      ].join("\n\n * ") + "\n\n"
     end
   end
 end
