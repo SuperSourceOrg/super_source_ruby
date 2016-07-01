7ee51ef34b7d5e49339833c384bd1bffe0df6888
J. R. Pickhardt <pickhardt@gmail.com>
2016-07-01 00:00:11 -0700

    Return if no config path

--- lib/supported_source/config.rb
+++ lib/supported_source/config.rb
@@ -1,6 +1,10 @@
 module SupportedSource
   class Config
     def self.load_config!
+      if !SupportedSource.project_supso_config_root
+        return
+      end
+
       configs_to_load = ["/config.json"]
       configs_to_load.each do |relative_path|
         config_path = SupportedSource.project_supso_config_root + relative_path
